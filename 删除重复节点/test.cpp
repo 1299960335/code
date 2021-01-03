13b5dd00c8207adfac495e0c2ea96719994b1044
1299960335 <1299960335@qq.com>
2021-01-03 23:29:48 +0800

    删除链表中的重复节点

---
+++ 删除重复节点/test.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* deleteDuplication(ListNode* pHead)
+    {
+        ListNode *vhead = new ListNode(-1);
+        vhead->next = pHead;
+        ListNode *pre = vhead, *cur = pHead;        
+        while (cur) {
+            if (cur->next && cur->val == cur->next->val) {
+                cur = cur->next;
+                while (cur->next && cur->val == cur->next->val) {
+                    cur = cur->next;
+                }
+                cur = cur->next;
+                pre->next = cur;
+            }
+            else {
+                pre = cur;
+                cur = cur->next;
+            }
+        }
+        return vhead->next;
+    }
+};
